6270d78d31110f5d9d3f2e21077e5530232db72e
Yusun Park <solarpy@hanyang.ac.kr>
2023-08-16 14:24:37 +0900

    환경변수 외부설정 1 : 문자열로 이용

--- src/main/java/com/ktds/myspringboot/runner/MyRunner.java
+++ src/main/java/com/ktds/myspringboot/runner/MyRunner.java
@@ -1,14 +1,25 @@
 package com.ktds.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+
+    @Value("${myboot.name}")
+    private String name;
+    @Value("${myboot.age}")
+    private int age;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("VM Argument = " + args.containsOption("foo")); // false
         System.out.println("Program Argument = " + args.containsOption("bar")); // true
+        System.out.println("******환경변수*********");
+        System.out.println("myboot.name" + name);
+        System.out.println("myboot.age" + age);
+
     }
 }
