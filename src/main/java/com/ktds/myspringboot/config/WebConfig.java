f559473a1c3049a7812e4d29c36c6567aa745615
Yusun Park <solarpy@hanyang.ac.kr>
2023-08-18 15:37:00 +0900

    Spring Boot Web MVC : 정적 리소스 맵핑 커스터 마이징

---
+++ src/main/java/com/ktds/myspringboot/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ktds.myspringboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override // http://localhost:8080/mobile/index.html
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                //반드시 mobile 다음에 / 을 주어야 한다.
+                .addResourceLocations("classpath:/mobile/")
+                .setCachePeriod(20);//20초
+    }
+}
