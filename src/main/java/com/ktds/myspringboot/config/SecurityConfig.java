c1e715eef273d177b06a1ebb4a0d375ddaa22bc5
Yusun Park <solarpy@hanyang.ac.kr>
2023-08-18 17:26:21 +0900

    Security : Maria DB 설정

--- src/main/java/com/ktds/myspringboot/config/SecurityConfig.java
+++ src/main/java/com/ktds/myspringboot/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.ktds.myspringboot.config;
 
+import com.ktds.myspringboot.service.UserInfoUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,18 +28,23 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
 //authentication
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.withUsername("adminboot")
-                .password(encoder.encode("pwd1"))
-                .roles("ADMIN")
-                .build();
-        UserDetails user = User.withUsername("userboot")
-                .password(encoder.encode("pwd2"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserInfoUserDetailsService();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider
+                =
+                new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService
+                (userDetailsService());
+        authenticationProvider.setPasswordEncoder
+                (passwordEncoder());
+        return authenticationProvider
+                ;
     }
 
     // welcome 페이지 설정
@@ -44,7 +52,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(new AntPathRequestMatcher("/users/welcome")).permitAll()
+                    auth.requestMatchers(new AntPathRequestMatcher("/users/welcome"), new AntPathRequestMatcher("/userinfos/new")).permitAll()
                             .requestMatchers(new AntPathRequestMatcher("/users/**")).authenticated();
                 })
                 .formLogin(withDefaults())
---
+++ src/main/java/com/ktds/myspringboot/repository/UserInfoRepository.java
@@ -0,0 +1,10 @@
+package com.ktds.myspringboot.repository;
+
+import com.ktds.myspringboot.entity.UserInfo;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends ListCrudRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email);
+}
