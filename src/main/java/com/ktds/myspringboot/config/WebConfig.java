6a14df5d85fd2187de38d00ffb9997fb2c0f0ba1
Yusun Park <solarpy@hanyang.ac.kr>
2023-08-18 15:43:48 +0900

    Spring Boot Web MVC : CORS Config 설정

--- src/main/java/com/ktds/myspringboot/config/WebConfig.java
+++ src/main/java/com/ktds/myspringboot/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ktds.myspringboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,4 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/mobile/")
                 .setCachePeriod(20);//20초
     }
+
+    @Override // CORS 포트 열어주는 설정
+    // -> 이렇게 config로 해주면 각 controller마다 @CrossOrigin(origins="http://localhost:18080")를 안넣어도된다.
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:18080")
+                .allowedMethods("*");;
+    }
+
 }
